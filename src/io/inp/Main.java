5475c01b4946cd38c08db1704d106b54b4a49468
Ashley Williamson <11356993@students.lincoln.ac.uk>
2016-08-07 19:02:19 +0100

    Changed Main comment for QLearning instantiation

--- src/io/inp/Main.java
+++ src/io/inp/Main.java
@@ -3,7 +3,7 @@ package io.inp;
 public class Main {
 
     public static void main(String[] args) {
-        // double init_epsilon, double final_epsilon, int explore, int episodes, double gamma
+        // double init_epsilon, double final_epsilon, int episodes, double alpha, double gamma.
         QLearning qLearn = new QLearning( 0.1, 0.0001, 50000, 0.5, 0.9 );
         qLearn.run();
 
