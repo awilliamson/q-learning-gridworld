c467002f96acae211904066492f6b039da9e2bec
Ashley Williamson <11356993@students.lincoln.ac.uk>
2016-08-07 18:33:13 +0100

    Change Q Learning initial parameters
    Increased P( epsilon )
    Increased lower-bound on P( epsilon )
    Reduced #Episodes
    Reduced Gamma 1.0 -> 0.9, to prevent non-discounted Q Learning.

--- src/io/inp/Main.java
+++ src/io/inp/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // double init_epsilon, double final_epsilon, int explore, int episodes, double gamma
-        QLearning qLearn = new QLearning( 0.001, 0.00001, 3000000, 1.00 );
+        QLearning qLearn = new QLearning( 0.1, 0.0001, 30000, 0.9 );
         qLearn.run();
 
         System.out.println( "Final" );
